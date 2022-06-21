3db83e6b10ac05195881ab9d28d750919f5427cf
asj <dkstmdwl0615@naver.com>
2022-06-21 13:05:14 +0900

    react 1st day mid-closure

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const posts = [
+    {id:0, title : 'react js 1st day',wr_date: new Date('2022-06-21').toString()},
+    {id:1, title : 'firebase 입문',wr_date: new Date('2022-06-30').toString()},
+    {id:2, title : 'HTML/CSS web_design',wr_date: new Date('2022.06-01').toString()},
+    {id:3, title : 'adobe photoshop / illust cs6',wr_date: new Date('2022.06-01').toString()},
+    {id:4, title : 'clip studio paint',wr_date: new Date('2022.06-01').toString()},
+  ]
+
+  const renderPosts = posts.map((v,i) =>{
+    return (
+      <div className='rows' key={v.id}>
+        <div className='row'>{v.title} <span className='date'>{v.wr_date}</span></div>
+      </div>
+    )
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <h1>front-end Designer</h1>
+        </header>
+     <section>
+      <ul>
+        {/* {posts} */}
+        {renderPosts}
+      </ul>
+     </section>
     </div>
   );
 }
